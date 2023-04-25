7591ca0a52db34087fe49eaaba10811272b692af
Carlos Elandro <carlos.wx290@gmail.com>
2023-04-25 09:41:23 -0300

    feat: implementação mudança de telas

--- AStar3.py
+++ AStar3.py
@@ -1,4 +1,4 @@
-#Imports dos mapas e das bibliotecas necessárias
+# Imports dos mapas e das bibliotecas necessárias
 import pygame
 import time
 from mapa import mapa
@@ -8,48 +8,48 @@ from dungeon3 import dungeon3
 from queue import PriorityQueue
 
 
-#Cores do mapa
+# Cores do mapa
 CORES = {
     'Branco': (255,255,255),
-    #Terrenos
-    'Grama': (144,238,144), #Grama-> Verde Claro
-    'Areia': (245,222,179), #Areia -> Marrom Claro
-    'Floresta': (34,139,34), #Floresta -> Verde Escuro
-    'Montanha': (139,69,19),  #Montanha -> Marrom escuro
-    'Agua': (0,0,139), #Água -> Azul Escuro
+    # Terrenos
+    'Grama': (144,238,144), # Grama-> Verde Claro
+    'Areia': (245,222,179), # Areia -> Marrom Claro
+    'Floresta': (34,139,34), # Floresta -> Verde Escuro
+    'Montanha': (139,69,19),  # Montanha -> Marrom escuro
+    'Agua': (0,0,139), # Água -> Azul Escuro
 
-    #Cores das dungeons
-    'Parede': (139,69,19), #Marrom escuro
-    'Caminho': (245,222,179), #Marrom Claro
+    # Cores das dungeons
+    'Parede': (139,69,19), # Marrom escuro
+    'Caminho': (245,222,179), # Marrom Claro
 
-    #Cores dos demais elementos
-    'PortaDungeon': (0,0,0), #Preto
-    'PingenteD1': (0,0,255), #Azul
-    'PingenteD2': (0,255,0), #Verde
-    'PingenteD3': (255,0,0), #Vermelho
-    'Grade': (128, 128, 128), #Cinza
-    'PontoPartida': (47,79,79), #Casa do Link -> Verde Acinzentado
-    'PontoFinal': (192,192,192), #Master Sword -> Prata
-    'CaminhoPercorrido': (255,165,0), #Caminho Final -> Laranja
+    # Cores dos demais elementos
+    'PortaDungeon': (0,0,0), # Preto
+    'PingenteD1': (0,0,255), # Azul
+    'PingenteD2': (0,255,0), # Verde
+    'PingenteD3': (255,0,0), # Vermelho
+    'Grade': (128, 128, 128), # Cinza
+    'PontoPartida': (47,79,79), # Casa do Link -> Verde Acinzentado
+    'PontoFinal': (192,192,192), # Master Sword -> Prata
+    'CaminhoPercorrido': (255,165,0), # Caminho Final -> Laranja
 }
 
-#Globais
+# Globais
 tela_criada = False
 global tela
 TAMANHO_CELULA = 15
 
-#Inicializa o Pygame
+# Inicializa o Pygame
 pygame.init()
 
 def Desenha_mapa(matriz_atual):
     global tela_criada
     global tela
 
-    #Definição de valores do mapa principal
+    # Definição de valores do mapa principal
     linhas = 42
     colunas = 42
 
-    #Propriedades da tela
+    # Propriedades da tela
     LARGURA_TELA = colunas * TAMANHO_CELULA
     ALTURA_TELA = linhas * TAMANHO_CELULA
 
@@ -58,10 +58,11 @@ def Desenha_mapa(matriz_atual):
         tela = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA))
         tela_criada = True
 
+    # Percorre a matriz atribuindo cor e pintando os elementos do mapa
     for linha in range(len(matriz_atual)):
         for coluna in range(len(matriz_atual[linha])):
             valor = matriz_atual[linha][coluna]
-            #Pintando terrenos do mapa
+            # Pintando terrenos do mapa
             if valor == 10:
                 cor = CORES['Grama']
             elif valor == 20:
@@ -73,7 +74,7 @@ def Desenha_mapa(matriz_atual):
             elif valor == 180:
                 cor = CORES['Agua']
 
-            #Adiciona pontos de interesse
+            # Adiciona pontos de interesse
             if valor == 99:
                 cor = CORES["PontoPartida"]
 
@@ -83,7 +84,7 @@ def Desenha_mapa(matriz_atual):
             elif valor == 94:
                 cor = CORES["PontoFinal"]
 
-            #Chamadas das funções de desenho
+            # Chamada das funções de desenho
             Desenha_celula(cor, linha, coluna, TAMANHO_CELULA)
             Desenha_linha(linha, coluna, TAMANHO_CELULA)
 
@@ -94,11 +95,11 @@ def Desenha_dungeon(matriz_atual):
     global tela_criada
     global tela
 
-    #Definição de linhas e colunas das dungeons
+    # Definição de linhas e colunas das dungeons
     linhas = 28
     colunas= 28
 
-    #Propriedades da tela
+    # Propriedades da tela
     LARGURA_TELA = colunas * TAMANHO_CELULA
     ALTURA_TELA = linhas * TAMANHO_CELULA
 
@@ -108,15 +109,16 @@ def Desenha_dungeon(matriz_atual):
         tela = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA))
         tela_criada = True
 
+    # Percorre a matriz atribuindo cor e pintando os elementos do mapa
     for linha in range(len(matriz_atual)):
         for coluna in range(len(matriz_atual[linha])):
             valor = matriz_atual[linha][coluna]
-            if valor == 1000:
+            if valor == 0:
                 cor = CORES['Parede']
             elif valor == 10:
                 cor = CORES['Caminho']
 
-            #Pintando a posição da porta e do pingente
+            # Pintando a posição da porta e do pingente
             if(valor == 1):
                 cor = CORES['PortaDungeon']
 
@@ -129,6 +131,7 @@ def Desenha_dungeon(matriz_atual):
             if((matriz_atual == dungeon3) and (valor == 2)) :
                 cor = CORES['PingenteD3']
 
+            # Chamada das funções de desenho
             Desenha_celula(cor, linha, coluna, TAMANHO_CELULA)
             Desenha_linha(linha, coluna, TAMANHO_CELULA)
 
@@ -160,13 +163,13 @@ def heuristica(ponto1, ponto2, matriz_atual):
 def adiciona_vizinhos(matriz, pos):
     vizinhos = []
     x, y = pos
-    if x < len(matriz) - 1 and matriz[x+1][y] != 1:
+    if x < len(matriz) - 1 and matriz[x+1][y] != 0: # Verifica Direita
         vizinhos.append((x+1, y))
-    if x > 0 and matriz[x-1][y] != 1:
+    if x > 0 and matriz[x-1][y] != 0:   # Verifica Esquerda
         vizinhos.append((x-1, y))
-    if y < len(matriz[0]) - 1 and matriz[x][y+1] != 1:
+    if y < len(matriz[0]) - 1 and matriz[x][y+1] != 0: # Verifica Baixo
         vizinhos.append((x, y+1))
-    if y > 0 and matriz[x][y-1] != 1:
+    if y > 0 and matriz[x][y-1] != 0:   # Verifica Cima
         vizinhos.append((x, y-1))
     return vizinhos
 
@@ -176,12 +179,12 @@ def algoritmo(matriz_atual, ponto1, ponto2):
     lista_aberta = PriorityQueue()
     lista_aberta.put((0, count, ponto1))
     caminho_percorrido = {}
-    g_score = {(x, y): float("inf") for x in range(len(matriz_atual)) for y in range(len(matriz_atual[0]))}
-    g_score[ponto1] = 0
-    f_score = {(x, y): float("inf") for x in range(len(matriz_atual)) for y in range(len(matriz_atual[0]))}
-    f_score[ponto1] = heuristica(ponto1, ponto2, matriz_atual)
+    g = {(x, y): float("inf") for x in range(len(matriz_atual)) for y in range(len(matriz_atual[0]))}
+    g[ponto1] = 0
+    f = {(x, y): float("inf") for x in range(len(matriz_atual)) for y in range(len(matriz_atual[0]))}
+    f[ponto1] = heuristica(ponto1, ponto2, matriz_atual)
 
-    open_set_hash = {ponto1}
+    conjunto_aberto = {ponto1}
 
     while not lista_aberta.empty():
         for event in pygame.event.get():
@@ -189,22 +192,22 @@ def algoritmo(matriz_atual, ponto1, ponto2):
                 pygame.quit()
 
         atual = lista_aberta.get()[2]
-        open_set_hash.remove(atual)
+        conjunto_aberto.remove(atual)
 
         if atual == ponto2:
             break
 
         for vizinho in adiciona_vizinhos(matriz_atual, atual):
-            temp_g_score = g_score[atual] + matriz_atual[vizinho[0]][vizinho[1]]
+            temp_g = g[atual] + matriz_atual[vizinho[0]][vizinho[1]]
 
-            if temp_g_score < g_score[vizinho]:
+            if temp_g < g[vizinho]:
                 caminho_percorrido[vizinho] = atual
-                g_score[vizinho] = temp_g_score
-                f_score[vizinho] = temp_g_score + heuristica(vizinho, ponto2, matriz_atual)
-                if vizinho not in open_set_hash:
+                g[vizinho] = temp_g
+                f[vizinho] = temp_g + heuristica(vizinho, ponto2, matriz_atual)
+                if vizinho not in conjunto_aberto:
                     count += 1
-                    lista_aberta.put((f_score[vizinho], count, vizinho))
-                    open_set_hash.add(vizinho)
+                    lista_aberta.put((f[vizinho], count, vizinho))
+                    conjunto_aberto.add(vizinho)
 
         if(matriz_atual == mapa):
             Desenha_mapa(matriz_atual)
@@ -223,7 +226,7 @@ def algoritmo(matriz_atual, ponto1, ponto2):
         Desenha_caminho(matriz_atual, caminho_final)
 
 
-    time.sleep(20)
+    time.sleep(3)
     return False
 
 
@@ -281,43 +284,50 @@ while run:
         if evento.type == pygame.QUIT:
             run = False
 
-    # if(pingenteD1Bool == False):
-    #     #Ponto de origem -> Porta Dungeon1
-    #     algoritmo(mapa, ponto_origem, portaD1)
-    #     # #Entrada da Dungeon1 -> Pingente 1
-    #     # algoritmo(dungeon1, entradaD1, pingente1)
-    #     # #Caminho inverso
-    #     # algoritmo(dungeon1, pingente1, entradaD1)
-    #     # pingenteD1Bool = True
+    if(pingenteD1Bool == False):
+        #Ponto de origem -> Porta Dungeon1
+        algoritmo(mapa, ponto_origem, portaD1)
+        tela_criada = False
+        #Entrada da Dungeon1 -> Pingente 1
+        algoritmo(dungeon1, entradaD1, pingente1)
+        tela_criada = False
+        #Caminho inverso
+        algoritmo(dungeon1, pingente1, entradaD1)
+        tela_criada = False
+        #Pingente 2 Adquirido
+        pingenteD1Bool = True
 
-    # if pingenteD2Bool == False:
-    #     #Entrada Dungeon1 -> Entrdada Dungeon2
-    #     algoritmo(mapa, portaD1, portaD2)
-    #     #Entrada da Dungeon2 -> Pingente 2
-    #     algoritmo(dungeon2, entradaD2, pingente2)
-    #     #Caminho inverso
-    #     algoritmo(dungeon2, pingente2, entradaD2)
-    #     pingenteD2Bool = True
+        if pingenteD2Bool == False:
+            #Entrada Dungeon1 -> Entrdada Dungeon2
+            algoritmo(mapa, portaD1, portaD2)
+            tela_criada = False
+            #Entrada da Dungeon2 -> Pingente 2
+            algoritmo(dungeon2, entradaD2, pingente2)
+            tela_criada = False
+            #Caminho inverso
+            algoritmo(dungeon2, pingente2, entradaD2)
+            tela_criada = False
+            # Pingente 2 Adquirido
+            pingenteD2Bool = True
 
-    # if pingenteD3Bool == False:
-    #     #Entrada Dungeon2 -> Entrdada Dungeon3
-    #     algoritmo(mapa, portaD2, portaD3)
-    #     #Entrada da Dungeon3 -> Pingente 3
-    #     algoritmo(dungeon3, entradaD3, pingente3)
-    #     #Caminho inverso
-    #     algoritmo(dungeon3, pingente3, entradaD3)
-    #     pingenteD3Bool == True
+            if pingenteD3Bool == False:
+                #Entrada Dungeon2 -> Entrdada Dungeon3
+                algoritmo(mapa, portaD2, portaD3)
+                tela_criada = False
+                #Entrada da Dungeon3 -> Pingente 3
+                algoritmo(dungeon3, entradaD3, pingente3)
+                tela_criada = False
+                #Caminho inverso
+                algoritmo(dungeon3, pingente3, entradaD3)
+                tela_criada = False
+                # Pingente 3 Adquirido
+                pingenteD3Bool == True
 
-    # if masterSwordBool == False:
-    #     #Entrada Dungeon3 -> Ponto de destino
-    #     algoritmo(mapa, portaD3, ponto_destino)
-    #     masterSwordBool = True
-    #     break
+                if masterSwordBool == False:
+                    #Entrada Dungeon3 -> Ponto de destino
+                    algoritmo(mapa, portaD3, ponto_destino)
+                    masterSwordBool = True
+                    break
 
-    #Desenha_mapa(mapa)
-    #Desenha_dungeon(dungeon1)
-    # ponto1 = (27,24)
-    # ponto2 = (17,39)
-
-    algoritmo(mapa, ponto_origem, portaD1)
+    algoritmo(dungeon1, entradaD1, pingente1)
     #break
